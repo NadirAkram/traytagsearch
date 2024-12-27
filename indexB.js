a0c7fd9ff113a697a06dd0bf77a045005e89b06e
NadirAkram <97799221+NadirAkram@users.noreply.github.com>
2024-12-27 22:38:48 +0530

    Update and rename index.js to indexB.js

--- indexB.js
+++ indexB.js
@@ -11,15 +11,15 @@ document.getElementById("search").onclick = function() {
 	tags = document.getElementById("tag").value;
 	from = document.getElementById("from").value;
 	const fromToArray = from.split("+");
-	const yourArray = tags.split("; ");
+	const yourArray = tags.split(",");
 	const count = yourArray.length;
 	const list = [];
 	for (let i = 0; i < count; i++) {
-		const string1 = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="lcldrevit_parameter_Raceway Sequence ID_PG_IDENTITYDATA">Raceway Sequence ID</name> </property> <value> <data type="wstring">${yourArray[i]}</data> </value> </condition>`;
+		const string1 = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="lcldrevit_parameter_Raceway Sequence ID_PG_IDENTITYDATA">Tag Number</name> </property> <value> <data type="wstring">${yourArray[i]}</data> </value> </condition>`;
 		list.push(string1);
 	}
 	const newString = list.join("\n");
-	const fromtostring = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Name</name> </property> <value> <data type="wstring">${fromToArray[0]}</data> </value> </condition> <condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Name</name> </property> <value> <data type="wstring">${fromToArray[1]}</data> </value> </condition>`
+	const fromtostring = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Name</name> </property> <value> <data type="wstring">${fromToArray[0]}</data> </value> </condition> <condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Equipment Number</name> </property> <value> <data type="wstring">${fromToArray[1]}</data> </value> </condition>`
 	const finalString = `<?xml version="1.0" encoding="UTF-8" ?> <exchange xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://download.autodesk.com/us/navisworks/schemas/nw-exchange-12.0.xsd" units="ft" filename="" filepath=""> <findspec mode="all" disjoint="0"> <conditions>${newString}${fromtostring} </conditions> <locator>/</locator> </findspec> </exchange>`;
 	document.getElementById("myInput").textContent = `${finalString}`
 }
